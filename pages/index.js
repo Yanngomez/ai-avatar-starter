800a99075d81b160131c92fdb4eb3566eb451159
Yann Gomez <65024536+Yanngomez@users.noreply.github.com>
2023-03-16 00:19:46 +0100

    Commit du 15/03/23

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,78 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  // Create state property
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState('');
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = async () => {
+    console.log('Generating...');	
+    // Add the fetch request
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    // If model still loading, drop that retry time
+    if (response.status === 503) {
+      console.log('Model is loading still :(.')
+      return;
+    }
+
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+    // Set image data into state property
+    setImg(data.image);
+
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator | Yann GOMEZ</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Yann Gomez picture generator</h1>
           </div>
           <div className="header-subtitle">
             <h2>description of your generator</h2>
+          </div>
+          {/* Add prompt container here */}
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange}/>
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={generateAction}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-avatar"
+          href="https://www.linkedin.com/in/yann-gomez2000/"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>build by Yann Gomez</p>
           </div>
         </a>
       </div>
